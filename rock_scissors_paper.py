f17d441b6675a9e1450d37f335e326df6165cb28
Patrick000000 <43682578+Patrick000000@users.noreply.github.com>
2019-09-19 08:54:49 -0400

    Update rock_scissors_paper.py

--- rock_scissors_paper.py
+++ rock_scissors_paper.py
@@ -4,7 +4,31 @@ Rock, Scissors, Paper
 Group members:
 """
 
+import random
 
-def some_function():
-    return True
+Draw = True
+
+#Base code
+
+def decider(comp, human):
+    global Draw
+    Win = "You win"
+    Lose = "You Lose"
+    Dra = "Draw"
+    if comp == human:
+        return Dra
+    if comp == 'Rock' and human == 'Paper' or comp == 'Scissors' and human == 'Rock' or comp == 'Paper' and human == 'Scissors':
+        Draw = False
+        return Win
+    if comp == 'Paper' and human == 'Rock' or comp == 'Rock' and human == 'Scissors' or comp == 'Scissors' and human == 'Paper':
+        Draw = False
+        return Lose
+
+while Draw is True:
+    inp = input('Please enter your choice: ')
+    choice = int(random.randint(0, 2))
+    RPS = ['Rock', 'Paper', 'Scissors']
+    print(RPS[choice])
+    Answer = decider(RPS[choice], inp)
+    print("\n" + Answer)
 
